a42469c548e9695b788c9e9a03214552437211b8
viml kumar <vimalkg@gmail.com>
2024-12-02 14:59:16 +0530

    zava8.vercel.app fb.com/ztrabc

--- src/ztr8/ue_to_il.js
+++ src/ztr8/ue_to_il.js
@@ -4,7 +4,7 @@ export function ue_to_il(ioz) {
 	ioz.z.value = "e_to_i"; duztr.doztr(ioz); ioz.i.value = ioz.o.value;
 	let all_text = '';
 	ioz.z.value = "u_to_i"; duztr.doztr(ioz);
-	all_text += ('0. ' + ioz.o.value + " #làtinнinԃi\neng:52:a-z+A-Z ing:31:a-z+ᴀԃнṅᴛ (ᴀ=à=A≠a ԃ=D≠d H≠h ṅ=N=ń≠n ᴛ≠t c=ꞇ=ć=च  ch=छ) zava8.github.io -> #font -> ing115.ttf\n") ;
+	all_text += ('0. ' + ioz.o.value + " #làtinнinԃi\neng:52:a-z+A-Z ing:31:a-z+àԃнńᴛ (ᴀ=à=A≠a ԃ=D≠d H≠h ṅ=N=ń≠n ᴛ≠t c=ꞇ=ć=च  ch=छ) zava8.github.io -> #font -> ing115.ttf\n") ;
 	ioz.z.value = "i_to_il"; duztr.doztr(ioz);
 	all_text += (ioz.o.value + "\n") ;
 	ioz.o.value = all_text;
